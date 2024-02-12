15b803c780a06d35845636be05f5a2ef4f343a7d
Kevin Savonije <kevin@23g.nl>
2024-02-12 21:08:27 +0100

    init

---
+++ src/components/Add.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// needs scripts
+</script>
+
+<template>
+  <h1>DVD Database</h1>
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Add from '../components/Add.vue'
+</script>
+
+<template>
+  <main>
+    <Add />
+  </main>
+</template>
