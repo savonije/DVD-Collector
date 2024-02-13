64a1f36b47aef146db8ca11df0ec8b4450eb0b61
Kevin Savonije <kevin@23g.nl>
2024-02-13 21:50:19 +0100

    Typing

--- src/components/ShowTitles.vue
+++ src/components/ShowTitles.vue
@@ -3,7 +3,12 @@ import { onMounted, ref, type Ref } from 'vue'
 import MovieCard from './MovieCard.vue'
 import axios from 'axios'
 
-const items: Ref<Array<T> | undefined> = ref([])
+interface Movie {
+  id: string
+  name: string
+}
+
+const items: Ref<Movie[] | undefined> = ref([])
 
 const loadTitles = () => {
   axios
@@ -12,8 +17,7 @@ const loadTitles = () => {
       for (const id in response.data) {
         items.value.push({
           id: id,
-          name: response.data[id].name,
-          rating: response.data[id].rating
+          name: response.data[id].name
         })
       }
     })
