beda73fb0dcdeca4b6a3aac92ce8ace3c3606569
Kevin Savonije <kevin@23g.nl>
2024-02-25 00:10:04 +0100

    Rating styling

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,15 +1,37 @@
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+
 const props = defineProps<{
   id: string
   name: string
   rating: number
 }>()
+
+const ratingColor = ref('')
+
+const getRatingBgColor = (rating: number) => {
+  if (rating < 5) {
+    ratingColor.value = 'bg-red'
+  } else if (rating < 7) {
+    ratingColor.value = 'bg-orange-600'
+  } else {
+    ratingColor.value = 'bg-green-600'
+  }
+}
+
+onMounted(() => {
+  getRatingBgColor(props.rating)
+})
 </script>
 
 <template>
-  <RouterLink :to="`/movie/${name}/${id}`" class="bg-white shadow mb-3 p-3 flex gap-3">
-    <img src="https://placehold.it/100x150" width="100" height="150" />
+  <RouterLink
+    :to="`/movie/${name}/${id}`"
+    class="bg-white shadow hover:shadow-md transition-shadow mb-3 p-3 flex gap-3 justify-between"
+  >
     <h4>{{ props.name }}</h4>
-    <span>{{ props.rating }}</span>
+    <span :class="`w-7 h-7 text-white font-bold flex items-center justify-center ${ratingColor}`">
+      {{ props.rating }}
+    </span>
   </RouterLink>
 </template>
