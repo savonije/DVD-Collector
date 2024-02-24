e8f653c6c632109a1e99fad238354eb06f9bf4e7
Kevin Savonije <kevin@23g.nl>
2024-02-24 21:27:58 +0100

    Empty state for no DVDs found

--- src/components/ShowTitles.vue
+++ src/components/ShowTitles.vue
@@ -3,14 +3,21 @@ import MovieCard from '@/components/MovieCard.vue'
 import { useStoreDVDs } from '@/stores/storeDVDs'
 
 const items = useStoreDVDs()
+
+console.log(items.DVDs)
 </script>
 
 <template>
-  <MovieCard
-    v-for="item in items.DVDs"
-    :key="item.id"
-    :id="item.id"
-    :name="item.name"
-    :rating="item.rating"
-  />
+  <div v-if="items.DVDs.length > 0">
+    <MovieCard
+      v-for="item in items.DVDs"
+      :key="item.id"
+      :id="item.id"
+      :name="item.name"
+      :rating="item.rating"
+    />
+  </div>
+  <div v-else>
+    <h1>No DVDs found...</h1>
+  </div>
 </template>
