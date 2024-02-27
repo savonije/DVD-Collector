8c4b64a8cead3d68386192cf3d0e3aabab1b38b3
Kevin Savonije <kevin@23g.nl>
2024-02-27 21:51:50 +0100

    Movie Cards styling

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -24,13 +24,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <RouterLink
-    :to="`/movie/${name}/${id}`"
-    class="bg-white shadow hover:shadow-md transition-shadow mb-3 p-3 flex gap-3 justify-between"
-  >
+  <RouterLink :to="`/movie/${name}/${id}`" class="movie-card">
     <h4>{{ props.name }}</h4>
-    <span :class="`w-7 h-7 text-white font-bold flex items-center justify-center ${ratingColor}`">
+    <span :class="`rating ${ratingColor}`">
       {{ props.rating }}
     </span>
   </RouterLink>
 </template>
+
+<style lang="scss" scoped>
+.movie-card {
+  @apply relative bg-white shadow hover:shadow-md transition-shadow mb-3 p-3 flex gap-3 justify-end md:h-[250px] lg:h-[350px] rounded-lg flex-col aspect-3/4 flex-shrink-0;
+
+  &:before {
+    @apply absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/5 content-[''] rounded-lg;
+  }
+
+  .rating {
+    @apply absolute right-3 top-3 w-7 h-7 text-white font-bold flex items-center justify-center;
+  }
+}
+</style>
