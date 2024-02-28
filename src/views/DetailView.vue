a0ce3c27842d0c7ae4aab0983915f49fed276950
Kevin Savonije <kevin@23g.nl>
2024-02-28 07:41:22 +0100

    Check if movie data is loading

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -14,6 +14,7 @@ const name = route.params.name as string
 const queryName = name.split(' ').join('+')
 
 const movieDetails: Ref<MovieDetails | null> = ref(null)
+const isLoading = ref(true)
 
 const getInfo = () => {
   axios
@@ -25,6 +26,9 @@ const getInfo = () => {
     })
     .catch((error) => {
       console.error(error)
+    })
+    .finally(() => {
+      isLoading.value = false
     })
 }
 
@@ -38,9 +42,10 @@ onMounted(() => {
   <div class="container">
     <h1 class="capitalize">{{ name }}</h1>
   </div>
+
   <div class="container">
     <div class="bg-white shadow p-6">
-      <div class="flex gap-6" v-if="movieDetails">
+      <div class="flex gap-6" v-if="!isLoading && movieDetails">
         <div>
           <figure class="flex-shrink-0 border-2" v-if="movieDetails.Poster">
             <img :src="movieDetails.Poster" :alt="name" width="300" height="441" />
@@ -83,11 +88,13 @@ onMounted(() => {
           </div>
         </div>
       </div>
+
       <div v-else>
         <p>No movie information found in database...</p>
       </div>
     </div>
   </div>
+
   <div class="container py-6">
     <div class="flex justify-between">
       <RouterLink to="/" class="button">Back to overview</RouterLink>
