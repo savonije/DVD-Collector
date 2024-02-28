97c004c419bbe2f8c31d690eec06d57c70d7e022
Kevin Savonije <kevin@23g.nl>
2024-02-28 07:57:05 +0100

    Navigation guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import DetailView from '@/views/DetailView.vue'
 import AuthView from '@/views/AuthView.vue'
+import { useStoreAuth } from '@/stores/storeAuth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,15 @@ const router = createRouter({
       component: AuthView
     }
   ]
+})
+
+router.beforeEach(async (to) => {
+  const storeAuth = useStoreAuth()
+  const userId = storeAuth.user?.id
+
+  if (!userId && to.name !== 'auth') {
+    return { name: 'auth' }
+  }
 })
 
 export default router
