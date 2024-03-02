52640cf248b6dc923d0dd7ccfbe728ca45c7b949
Kevin Savonije <kevin@23g.nl>
2024-03-02 14:01:27 +0100

    Fix header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -5,19 +5,21 @@ const storeAuth = useStoreAuth()
 </script>
 
 <template>
-  <header class="container bg-shark text-white flex justify-between p-3 items-center mb-6">
-    <RouterLink to="/">
-      <span class="text-2xl font-bold font-heading">Mescie's DVD collection</span>
-    </RouterLink>
+  <header class="bg-shark text-white flex justify-between p-3 items-center mb-6">
+    <div class="container">
+      <RouterLink to="/">
+        <span class="text-2xl font-bold font-heading">Mescie's DVD collection</span>
+      </RouterLink>
 
-    <button
-      type="button"
-      aria-label="Logout user"
-      class="button"
-      @click="storeAuth.logoutUser"
-      v-if="storeAuth.user?.id"
-    >
-      Logout
-    </button>
+      <button
+        type="button"
+        aria-label="Logout user"
+        class="button"
+        @click="storeAuth.logoutUser"
+        v-if="storeAuth.user?.id"
+      >
+        Logout
+      </button>
+    </div>
   </header>
 </template>
