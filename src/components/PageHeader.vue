dda5af54b8cb71c4ccf14defb0b032278d0f2f8f
Kevin Savonije <kevin@23g.nl>
2024-10-27 10:49:34 +0100

    refactor: move username to env file

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,13 +3,15 @@ import { useStoreAuth } from '@/stores/storeAuth'
 import AddTitle from '@/components/AddTitle.vue'
 
 const storeAuth = useStoreAuth()
+
+const username = import.meta.env.VITE_USERNAME
 </script>
 
 <template>
   <header class="bg-shark text-teal p-3 mb-6">
     <div class="container flex justify-between items-center">
       <RouterLink to="/">
-        <span class="text-2xl font-bold font-heading">Mescie's DVD collection</span>
+        <span class="text-2xl font-bold font-heading"> {{ username }}'s DVD collection </span>
       </RouterLink>
 
       <div class="flex gap-3">
