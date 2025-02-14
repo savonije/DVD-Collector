cb7ef30cc0232c997e2d7204183717dd7064c40b
Kevin Savonije <kevin@23g.nl>
2025-02-15 00:59:06 +0100

    feat: improve chunk sizing

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern'
-      }
-    }
-  }
-})
+    plugins: [vue(), vueJsx(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern',
+            },
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'vue-vendors': ['vue', 'vue-router'],
+                    firebase: [
+                        'firebase/app',
+                        'firebase/auth',
+                        'firebase/firestore',
+                    ],
+                    vendor: ['axios'],
+                },
+            },
+        },
+    },
+});
