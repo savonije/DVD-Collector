f5f158608adf06b7515e3b8a45c25b3dc56f761d
Kevin Savonije <kevin@23g.nl>
2025-02-22 08:10:04 +0100

    chore: remove edit title functionality

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-    import { nextTick, ref } from 'vue';
     import { useI18n } from 'vue-i18n';
     import { useRoute } from 'vue-router';
 
@@ -8,36 +7,14 @@
     import DefaultLayout from '@/layouts/DefaultLayout.vue';
 
     import { useStoreAuth } from '@/stores/storeAuth';
-    import { useStoreDVDs } from '@/stores/storeDVDs';
 
     const route = useRoute();
-    const StoreDVD = useStoreDVDs();
     const storeAuth = useStoreAuth();
 
     const { t } = useI18n();
 
     const id = route.params.id as string;
     const name = route.params.name as string;
-
-    const showEdit = ref(false);
-    const titleInput = ref(name);
-    const inputRef = ref<HTMLInputElement | null>(null);
-
-    const toggleEdit = async () => {
-        if (!storeAuth.user?.id) return;
-        showEdit.value = !showEdit.value;
-        await nextTick();
-        if (showEdit.value && inputRef.value) {
-            inputRef.value.focus();
-        }
-    };
-
-    const submitForm = () => {
-        if (name !== titleInput.value) {
-            StoreDVD.updateDVD(id, titleInput.value);
-        }
-        showEdit.value = false;
-    };
 </script>
 
 <template>
@@ -47,20 +24,10 @@
                 <div class="bg-teal p-6">
                     <div class="flex justify-end">
                         <h2
-                            v-if="!showEdit"
                             class="text-shark mb-0 text-3xl font-bold capitalize"
-                            @click="toggleEdit"
                         >
                             {{ name }}
                         </h2>
-                        <form v-else @submit.prevent="submitForm">
-                            <input
-                                ref="inputRef"
-                                v-model="titleInput"
-                                class="bg-teal text-shark font-heading p-3 text-right text-3xl font-bold capitalize"
-                                type="text"
-                            />
-                        </form>
                     </div>
                 </div>
                 <div class="p-6">
