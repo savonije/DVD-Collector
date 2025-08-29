309ecad4a7f4a3173d131a4679e2a410f6b4160d
Kevin Savonije <kevin@23g.nl>
2025-08-29 16:00:08 +0200

    feat: change document title on page change

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+const routes = [
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        component: () => import('@/views/404View.vue'),
+        meta: { title: 'Oops! 404 Not Found - DVD Collection' },
+    },
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('@/views/HomeView.vue'),
+        meta: { title: 'Home - DVD Collection' },
+    },
+    {
+        path: '/movie/:name/:id',
+        name: 'detail',
+        component: () => import('@/views/DetailView.vue'),
+        meta: { title: 'Movie Details - DVD Collection' },
+    },
+    {
+        path: '/login',
+        name: 'auth',
+        component: () => import('@/views/AuthView.vue'),
+        meta: { title: 'Login - DVD Collection' },
+    },
+];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     scrollBehavior(to, from, savedPosition) {
-        if (savedPosition) {
-            return savedPosition;
-        } else {
-            return { top: 0 };
-        }
+        if (savedPosition) return savedPosition;
+        return { top: 0 };
     },
-    routes: [
-        {
-            path: '/:pathMatch(.*)*',
-            name: 'not-found',
-            component: () => import('@/views/404View.vue'),
-        },
-        {
-            path: '/',
-            name: 'home',
-            component: () => import('@/views/HomeView.vue'),
-        },
-        {
-            path: '/movie/:name/:id',
-            name: 'detail',
-            component: () => import('@/views/DetailView.vue'),
-        },
-        {
-            path: '/login',
-            name: 'auth',
-            component: () => import('@/views/AuthView.vue'),
-        },
-    ],
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta?.title) {
+        document.title = (to.meta?.title as string) || 'DVD Collection';
+    } else {
+        document.title = 'DVD Collection';
+    }
+    next();
 });
 
 export default router;
