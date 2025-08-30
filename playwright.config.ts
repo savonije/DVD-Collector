47c65c68c7335fed6e6e074a9f4a9fae7ff76257
Kevin Savonije <kevin@23g.nl>
2025-08-30 19:24:58 +0200

    playwright config edit

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,61 +12,29 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
     testDir: './tests',
-    /* Maximum time one test can run for. */
-    timeout: 30 * 1000,
-    expect: {
-        /**
-         * Maximum time expect() should wait for the condition to be met.
-         * For example in `await expect(locator).toHaveText();`
-         */
-        timeout: 5000,
-    },
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    workers: process.env.CI ? 2 : undefined,
     reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-        actionTimeout: 0,
-        /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.CI
-            ? 'http://localhost:4173'
-            : 'http://localhost:5173',
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        baseURL: 'http://localhost:5173',
+        permissions: ['clipboard-read', 'clipboard-write'],
         trace: 'on-first-retry',
-
-        /* Only on CI systems run the tests headless */
-        headless: !!process.env.CI,
     },
 
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'chromium',
-            use: {
-                ...devices['Desktop Chrome'],
-            },
+            use: { ...devices['Desktop Chrome'], channel: 'chrome' },
         },
     ],
 
-    /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-    // outputDir: 'test-results/',
-
-    /* Run your local dev server before starting the tests */
     webServer: {
-        /**
-         * Use the dev server by default for faster feedback loop.
-         * Use the preview server on CI for more realistic testing.
-         * Playwright will re-use the local server if there is already a dev-server running.
-         */
-        command: process.env.CI ? 'npm run preview' : 'npm run dev',
-        port: process.env.CI ? 4173 : 5173,
+        command: process.env.CI
+            ? 'npx vite preview --port=5173'
+            : 'npm run dev',
+        url: 'http://localhost:5173',
         reuseExistingServer: !process.env.CI,
     },
 });
